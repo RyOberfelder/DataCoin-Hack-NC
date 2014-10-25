3e924687f8f5abf35fb505b7dde008454006d0fc
Jaret Dawson <jaret.dawson@gmail.com>
2014-10-25 23:03:48 +0000

    Adding Star model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :stars
+
   resources :users
 
   resources :data_coins
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141025225230) do
+ActiveRecord::Schema.define(version: 20141025230142) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "data_coins", force: true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "stars", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
