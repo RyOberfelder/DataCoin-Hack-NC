ea914bc86b61c62067bbd0c32a5aeae8ff8237ca
Jaret Dawson <jaret.dawson@gmail.com>
2014-10-26 01:54:03 +0000

    Fixing views

--- app/models/data_coin.rb
+++ app/models/data_coin.rb
@@ -1,3 +1,4 @@
 class DataCoin < ActiveRecord::Base
   has_many :stars
+  validates :name, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
   has_many :stars
+  validates :username, presence: true
 end
