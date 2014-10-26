839fd43a1621fad14926aa977aa6094c1d0f93fb
Jaret Dawson <jaret.dawson@gmail.com>
2014-10-26 02:09:56 +0000

    Migrating db

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141026011537) do
+ActiveRecord::Schema.define(version: 20141026020622) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: true do |t|
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
+    t.integer  "data_coin_id"
+  end
+
+  add_index "comments", ["data_coin_id"], name: "index_comments_on_data_coin_id", using: :btree
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
   create_table "data_coins", force: true do |t|
     t.string   "name"
